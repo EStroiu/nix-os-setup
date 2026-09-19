1b8e05b397d8ae994f7e1c654abaca8a84b0dabf
EStroiu <e.stroiu@vu.nl>
2026-09-19 23:38:51 +0200

    added a nice cursor

--- home/remarka/home.nix
+++ home/remarka/home.nix
@@ -35,6 +35,16 @@
       sansSerif = [ "JetBrains Mono" ];
     };
   };
+  
+  # Mouse Cursor
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 20;
+  
+    gtk.enable = true;
+    x11.enable = true;
+  };
 
   # Kitty terminal
   programs.kitty = {
